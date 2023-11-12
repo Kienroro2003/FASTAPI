e58662fe5901cca47238834d40079c39b2b8894f
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-11-13 05:04:19 +0700

    udpate more config

--- database.py
+++ database.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from datetime import time
-
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:12345678@localhost/fastapi'
+from .config import settings
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
